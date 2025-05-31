16d03f2086e5c6253b26eb27f3e462fef22045e5
Jeremy Funk <jeremyfunk@Jeremys-MacBook-Pro.local>
2025-05-31 13:50:51 +0200

    Fix social queries

--- apps/convex/convex/social.ts
+++ apps/convex/convex/social.ts
@@ -1,32 +1,23 @@
-import { v } from "convex/values"
-import { accountQuery } from "./middleware/withAccount"
+import { userQuery } from "./middleware/withUser"
 
-export const getFriends = accountQuery({
-	args: {
-		serverId: v.id("servers"),
-	},
+export const getFriends = userQuery({
+	args: {},
 	handler: async (ctx, args) => {
 		const friends = await ctx.db
 			.query("users")
-			.withIndex("by_accountId_serverId", (q) =>
-				q.eq("accountId", ctx.account.doc._id).eq("serverId", args.serverId),
-			)
+			.withIndex("by_server_id", (q) => q.eq("serverId", args.serverId))
 			.collect()
 
 		return friends
 	},
 })
 
-export const getMembers = accountQuery({
-	args: {
-		serverId: v.id("servers"),
-	},
+export const getMembers = userQuery({
+	args: {},
 	handler: async (ctx, args) => {
 		const friends = await ctx.db
 			.query("users")
-			.withIndex("by_accountId_serverId", (q) =>
-				q.eq("accountId", ctx.account.doc._id).eq("serverId", args.serverId),
-			)
+			.withIndex("by_server_id", (q) => q.eq("serverId", args.serverId))
 			.collect()
 
 		return friends
