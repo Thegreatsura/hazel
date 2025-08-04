eb8c0ef132ae12a6227e8a16c226f09f03ef4263
Makisuo <minetoaster12@gmail.com>
2025-08-05 01:29:30 +0200

    remove markdown pakcage

--- packages/markdown/src/base-components.tsx
+++
@@ -1,6 +0,0 @@
-import { For } from "solid-js"
-import type { Components } from "./types"
-
-export const Text: Components["text"] = (props) => (
-	<For each={[...props.node.value]}>{(char) => <span class="">{char}</span>}</For>
-)
