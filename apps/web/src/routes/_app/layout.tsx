0db367480d1a2f32568883624123aee77e9f1d89
Makisuo <minetoaster12@gmail.com>
2025-10-06 20:26:23 +0200

    feat: add org preloading

--- apps/web/src/routes/_app/layout.tsx
+++ apps/web/src/routes/_app/layout.tsx
@@ -1,8 +1,15 @@
 import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router"
+import { organizationCollection, organizationMemberCollection } from "~/db/collections"
 import { useAuth } from "~/providers/auth-provider"
 
 export const Route = createFileRoute("/_app")({
 	component: RouteComponent,
+	loader: async () => {
+		await organizationCollection.preload()
+		await organizationMemberCollection.preload()
+
+		return null
+	},
 })
 
 function RouteComponent() {
